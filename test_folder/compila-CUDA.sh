2eaeba1def9d8068f300977fada2937f1e73a631
pablogalvelahoz <pgalve@ucm.es>
2023-03-02 18:08:38 +0100

    a

--- test_folder/compila-CUDA.sh
+++ test_folder/compila-CUDA.sh
@@ -1,6 +1,9 @@
 
-rm $2
-pgfortran -Bstatic -Mcuda $1 -o $2
+file=$1
+exe=$2
+rm $exe
+pgfortran -Bstatic_pgi -Mcuda  $file -o $exe
+#pgfortran -fast -fastsse -Bstatic_pgi -Mlarge_arrays -mcmodel=medium -tp px -Mcuda=ptxinfo,fastmath,lineinfo -Minfo  -ta=tesla $file -o $exe
 
 #-Bstatic for static libraries
 #-Mcuda works when the code has no .cuf or .CUF extension
