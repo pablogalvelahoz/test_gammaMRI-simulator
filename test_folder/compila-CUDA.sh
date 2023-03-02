7668392495d2b5173ee8521fc27cc6efbf0bd2b0
pablogalvelahoz <pgalve@ucm.es>
2023-03-02 21:32:08 +0100

    n

--- test_folder/compila-CUDA.sh
+++ test_folder/compila-CUDA.sh
@@ -2,7 +2,7 @@
 file=$1
 exe=$2
 rm $exe
-pgfortran -Bstatic_pgi -Mcuda -Minfo $file -o $exe
+pgfortran -Bstatic_pgi -Mcuda -Minfo -static-nvidia -tp px -Mcuda=ptxinfo,fastmath,lineinfo,cuda11.0 -Minfo  -ta=tesla:cuda11.0,cc6,cc7.5 $file -o $exe
 #pgfortran -fast -fastsse -Bstatic_pgi -Mlarge_arrays -mcmodel=medium -tp px -Mcuda=ptxinfo,fastmath,lineinfo -Minfo  -ta=tesla $file -o $exe
 
 #-Bstatic for static libraries
