b30d3da598f6fd0af753d17269dd607a754f63df
pablogalvelahoz <pgalve@ucm.es>
2023-03-02 16:11:52 +0100

    Add v1 with gpu reconstructor

---
+++ test_ideal_gMRI_v1/genera_list_gammaMRI.sh
@@ -0,0 +1,21 @@
+magfile=$1
+time_step=$2
+nx=$3
+ny=$4
+nt=$5
+amlife=$6
+act=$7
+det_radius=$8
+det_height=$9
+xfov=$10
+yfov=$11
+act_file=$12
+ndetectors=$13
+a2=$14
+if_reco=1
+gammaMRI_exe=gammaMRI_det_v2.x
+#gammaMRI_exe=gammaMRI_det_v2_opt.x
+rm list_gammaMRI.raw
+echo ./$gammaMRI_exe $magfile $time_step $nx $ny $nt $amlife $act $det_radius $det_height $xfov $yfov $act_file $ndetectors $a2 $if_reco
+./$gammaMRI_exe $magfile $time_step $nx $ny $nt $amlife $act $det_radius $det_height $xfov $yfov $act_file $ndetectors $a2 $if_reco
+ls -lrht list_gammaMRI.raw
